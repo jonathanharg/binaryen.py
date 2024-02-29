a82cf7b01e274cc4aacb6964586dccb8d5722009
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2024-02-29 10:07:08 +0000

    Moved file

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import find_packages, setup
 
+# setup(
+#     packages=find_packages(),
+#     install_requires=["cffi"],
+#     package_data={"libbinaryen": ["libbinaryen/binaryen_cffi.cpython-311-darwin.so"]},
+#     # cffi_modules=["binaryen/libbinaryen/build.py:ffibuilder"],
+# )
 setup(
-    packages=find_packages(),
-    install_requires=["cffi"],
-    package_data={"libbinaryen": ["libbinaryen/binaryen_cffi.cpython-311-darwin.so"]},
-    # cffi_modules=["binaryen/libbinaryen/build.py:ffibuilder"],
+    ...,
+    setup_requires=["cffi>=1.16.1"],
+    cffi_modules=["binaryen/libbinaryen/build.py:ffibuilder"],
+    install_requires=["cffi>=1.16.1"],
 )
--- tests/file_test.py
+++
@@ -1,6 +0,0 @@
-import binaryen
-
-
-def test_complicated_features():
-    module = binaryen.Module()
-    assert module

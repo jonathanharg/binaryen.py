e1683e41a648771677f3b469c2ab00685c7f6c07
Jonathan Hargreaves <jhargreaves189@googlemail.com>
2024-03-05 22:57:17 +0000

    Github actions fixes

--- scripts/build_libbinaryen.sh
+++ scripts/build_libbinaryen.sh
@@ -44,7 +44,7 @@ if [[ "$platform" == "macos" ]]; then
     file="version_$BINARYEN_VERSION.tar.gz"
     url="https://github.com/WebAssembly/binaryen/archive/refs/tags/$file"
     wget -q -nc --no-check-certificate --content-disposition $url -P "./binaryen/libbinaryen"
-    tar -xzvf "./binaryen/libbinaryen/binaryen-$file" -C "./binaryen/libbinaryen/src" --strip-components=1
+    tar -xzf "./binaryen/libbinaryen/binaryen-$file" -C "./binaryen/libbinaryen/src" --strip-components=1
     cmake -S "./binaryen/libbinaryen/src" -B "./binaryen/libbinaryen/build/" -G Ninja \
         -DCMAKE_INSTALL_PREFIX="$lib_path" -DCMAKE_OSX_ARCHITECTURES=$arch \
         -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF \
@@ -54,7 +54,7 @@ else
     file="binaryen-version_$BINARYEN_VERSION-$arch-$platform.tar.gz"
     url="https://github.com/WebAssembly/binaryen/releases/download/version_$BINARYEN_VERSION/$file"
     wget -q -nc --no-check-certificate --content-disposition $url -P $lib_path
-    tar -xzvf $lib_path$file -C $lib_path --strip-components=1 $wildcards \
+    tar -xzf $lib_path$file -C $lib_path --strip-components=1 $wildcards \
         "binaryen-version_$BINARYEN_VERSION/include/binaryen-c.h" \
         "binaryen-version_$BINARYEN_VERSION/include/wasm-delegations.def" \
         "binaryen-version_$BINARYEN_VERSION/lib/*"

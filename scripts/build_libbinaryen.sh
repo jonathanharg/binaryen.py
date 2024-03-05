31dc9507d6e612bbc0408dc98e1e3170b5fe9ce7
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2024-03-05 21:37:50 +0000

    Building on MacOS

--- scripts/build_libbinaryen.sh
+++ scripts/build_libbinaryen.sh
@@ -35,15 +35,29 @@ fi
 
 arch=$(uname -m)
 lib_path="./binaryen/libbinaryen/$arch-$platform/"
-file="binaryen-version_$BINARYEN_VERSION-$arch-$platform.tar.gz"
-url="https://github.com/WebAssembly/binaryen/releases/download/version_$BINARYEN_VERSION/$file"
-
 mkdir -p $lib_path
-wget -nc --no-check-certificate --content-disposition $url -P $lib_path
-tar -xzvf $lib_path$file -C $lib_path --strip-components=1 $wildcards \
-    "binaryen-version_$BINARYEN_VERSION/include/binaryen-c.h" \
-    "binaryen-version_$BINARYEN_VERSION/include/wasm-delegations.def" \
-    "binaryen-version_$BINARYEN_VERSION/lib/*"
+
+if [[ "$platform" == "macos" ]]
+then
+    mkdir -p "./binaryen/libbinaryen/src/"
+    file="version_$BINARYEN_VERSION.tar.gz"
+    url="https://github.com/WebAssembly/binaryen/archive/refs/tags/$file"
+    wget -nc --no-check-certificate --content-disposition $url -P "./binaryen/libbinaryen"
+    tar -xzvf "./binaryen/libbinaryen/binaryen-$file" -C "./binaryen/libbinaryen/src" --strip-components=1
+    cmake -S "./binaryen/libbinaryen/src" -B "./binaryen/libbinaryen/build/" -G Ninja \
+        -DCMAKE_INSTALL_PREFIX="$lib_path" -DCMAKE_OSX_ARCHITECTURES=$arch \
+        -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF \
+        -DBUILD_STATIC_LIB=ON
+    cmake --build "./binaryen/libbinaryen/build/" -v --config Release --target install
+else
+    file="binaryen-version_$BINARYEN_VERSION-$arch-$platform.tar.gz"
+    url="https://github.com/WebAssembly/binaryen/releases/download/version_$BINARYEN_VERSION/$file"
+    wget -nc --no-check-certificate --content-disposition $url -P $lib_path
+    tar -xzvf $lib_path$file -C $lib_path --strip-components=1 $wildcards \
+        "binaryen-version_$BINARYEN_VERSION/include/binaryen-c.h" \
+        "binaryen-version_$BINARYEN_VERSION/include/wasm-delegations.def" \
+        "binaryen-version_$BINARYEN_VERSION/lib/*"
+fi
 
 echo "Running python script"
 python3 ./scripts/create_cdef.py

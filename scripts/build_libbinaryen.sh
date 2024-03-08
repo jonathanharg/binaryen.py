c89d72adbe3a26e9b3578bea7bb52c601bd7c973
Jonathan Hargreaves <32854503+jonathanharg@users.noreply.github.com>
2024-03-08 12:40:05 +0000

    Fixed macos cross compilation

--- scripts/build_libbinaryen.sh
+++ scripts/build_libbinaryen.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+root_path="./binaryen/libbinaryen"
+
+if grep -Fxq $BINARYEN_VERSION $root_path/version; then
+    # Existing version found
+    echo "Found existing binaryen installation, skipping."
+    echo "You may need to clear the binaryen/libbinaryen folder if errors occur"
+    exit 0
+fi
 
 arch=$(uname -m)
 
+# Set the platform = 'macos' | 'linux' | 'windows'
 case "$(uname -sr)" in
 
 Darwin*)
@@ -30,42 +39,44 @@ if [ -z ${BINARYEN_VERSION+x} ]; then
     exit 1
 fi
 
-lib_path="./binaryen/libbinaryen/$arch-$platform"
+lib_path="$root_path/$arch-$platform"
 mkdir -p $lib_path
-
-if grep -Fxq $BINARYEN_VERSION $lib_path/version; then
-    # Existing version found
-    echo "Found existing binaryen installation, skipping."
-    echo "You may need to clear the binaryen/libbinaryen folder if errors occur"
-    exit 0
-fi
 
 wildcards="--wildcards"
 
 if [[ "$platform" == "macos" ]]; then
+    # Wildcards are supported by default on macOS
     wildcards=""
 fi
 
 echo "Setting up binaryen for $arch $platform"
 
 if [[ "$platform" == "macos" ]]; then
-    echo "Building static binaryen for MacOS"
-    mkdir -p "./binaryen/libbinaryen/src/"
+    echo "Building static binaryen for macOS"
+    mkdir -p "$root_path/src/"
 
     file="version_$BINARYEN_VERSION.tar.gz"
     url="https://github.com/WebAssembly/binaryen/archive/refs/tags/$file"
 
-    wget -q -nc --no-check-certificate --content-disposition $url -P "./binaryen/libbinaryen"
+    wget -q -nc --no-check-certificate --content-disposition $url -P "$root_path"
     echo "Downloaded source code"
 
-    tar -xzf "./binaryen/libbinaryen/binaryen-$file" -C "./binaryen/libbinaryen/src" --strip-components=1
+    tar -xzf "$root_path/binaryen-$file" -C "$root_path/src" --strip-components=1
     echo "Extracted source code"
 
-    cmake -S "./binaryen/libbinaryen/src" -B "./binaryen/libbinaryen/build/" -G Ninja \
-        -DCMAKE_INSTALL_PREFIX="$lib_path" -DCMAKE_OSX_ARCHITECTURES=$arch \
+    echo "Building for Apple Silicon"
+    cmake -S "$root_path/src" -B "$root_path/build/" -G Ninja \
+        -DCMAKE_INSTALL_PREFIX=""$root_path/arm64-macos"" -DCMAKE_OSX_ARCHITECTURES="arm64" \
         -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF \
         -DBUILD_STATIC_LIB=ON
-    cmake --build "./binaryen/libbinaryen/build/" -v --config Release --target install
+    cmake --build "$root_path/build/" -v --config Release --target install
+
+    echo "Build for Intel"
+    cmake -S "$root_path/src" -B "$root_path/build/" -G Ninja \
+        -DCMAKE_INSTALL_PREFIX=""$root_path/x86_64-macos"" -DCMAKE_OSX_ARCHITECTURES="x86_64" \
+        -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_TOOLS=OFF \
+        -DBUILD_STATIC_LIB=ON
+    cmake --build "$root_path/build/" -v --config Release --target install
 else
     file="binaryen-version_$BINARYEN_VERSION-$arch-$platform.tar.gz"
     url="https://github.com/WebAssembly/binaryen/releases/download/version_$BINARYEN_VERSION/$file"
@@ -84,4 +95,4 @@ echo "Generating C def file"
 python3 ./scripts/create_cdef.py
 echo "Successfully generated C def file"
 
-echo "$BINARYEN_VERSION" >| $lib_path/version
+echo "$BINARYEN_VERSION" >| $root_path/version
--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup
 
 setup(
-    setup_requires=["cffi>=1.15.0"],
     cffi_modules=["binaryen/binaryen_build.py:ffibuilder"],
-    install_requires=["cffi>=1.15.0"],
 )
